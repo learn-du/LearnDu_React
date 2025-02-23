bad553bae044f5dbede8dc7fde6a34a0f772e132
learn-du <learndu2023@gmail.com>
2025-02-23 17:13:55 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
     setLoading(true); // Show loading state
 
     try {
-      const response = await axios.post("https://learndu-react.onrender.com/api/users/login", formData);
+      const response = await axios.post("https://learndu-services-backend.onrender.com/api/users/login", formData);
 
       if (response.status === 200) {
         alert("Login successful!");
