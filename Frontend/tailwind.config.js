38c32de568284e48abde1b700fa27081a057c8e7
learn-du <learndu2023@gmail.com>
2025-02-23 18:11:45 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -11,6 +11,16 @@ export default {
       'black': '#0c0a09',
       'grey': '#f3f4f6',
       'orange':'#f97316',
+      yellow': '#fbbf24',
+      'white': '#ffffff',
+      'black': '#0c0a09',
+      'grey': '#f3f4f6',
+      'orange':'#f97316',
+      'red':'#f87171',
+      'green':'#34d399',
+      'blue':'#3b82f6',
+      'purple':'#8b5cf6',
+      
       
     },
     extend: {},
